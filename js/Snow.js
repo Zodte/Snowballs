7f4600ed41442b027f7ebd9c0c6de60cc5b7342a
Zodte <pedro_viking@yahoo.com.br>
2015-11-24 09:04:08 +0000

    Finished Snow

--- js/Snow.js
+++ js/Snow.js
@@ -11,10 +11,10 @@ Snow.prototype.vel = 5;
 
 Snow.prototype.update = function(du){
 	if(this.cx < -10) return entityManager.KILL_ME_NOW;
-	var amount = 200;
+	var amount = 600;
 	var values = Math.cos((this.cx % amount-amount/2)/amount/2*Math.PI);
 	var values2 = Math.sin((this.cx % 200-100)/50*Math.PI*-1);
-    var nextY = this.cyStart + values * 10; 
+    var nextY = this.cyStart + values * 20; 
     this.cx -= this.vel* Math.cos((this.cx % amount-amount/2)/amount/2) * du ;
     this.cy = nextY;
 	
@@ -24,5 +24,5 @@ Snow.prototype.update = function(du){
 Snow.prototype.render = function(ctx){
 	this.sprite.scale = this.scale;
 	this.sprite.drawCentredAt(ctx, this.cx,this.cy,this.rotation);
-	this.sprite.scale = 1;
+	this.sprite.scale = 0.2;
 }
