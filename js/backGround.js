4485e443469daf1c9bf6d880f8bc907f58cb094a
Zodte <pedro_viking@yahoo.com.br>
2015-11-24 10:43:12 +0000

    Snow now has a random velocity

--- js/backGround.js
+++ js/backGround.js
@@ -10,20 +10,24 @@ backGround.prototype = new Entity();
 backGround.prototype.cx = 0;
 backGround.prototype.cy = 500;
 backGround.prototype.rotation = 0;
+backGround.prototype.snowVel = 2;
 
 backGround.prototype.update = function(du) {
 	//this.cx -= MAP_SPEED;
-	for(var i = 0; i < Math.floor(util.randRange(0,2)); i++){
+	if(this.lived % 1000 == 999) this.snowVel = util.randRange(1,8);
+	for(var i = 0; i < Math.floor(util.randRange(0,5)); i++){
 		entityManager.generateSnow({
-			cx : g_canvas.width,
-			cy : util.randRange(0,g_canvas.height)
+			cx : g_canvas.width+util.randRange(0,g_canvas.width),
+			cy : util.randRange(0,g_canvas.height*2)-g_canvas.height,
+			vel : this.snowVel
 		});
 	}
+	this.lived++;
 };
 
 backGround.prototype.render = function(ctx) {
 	for(var i = 0; i < 8; i++){
-		this.sprite.drawWrappedCentredAt(ctx,this.sprite.width/2 + i*this.sprite.width, this.sprite.height/2,this.rotation)
+		this.sprite.drawWrappedCentredAt(ctx,this.sprite.width/2 + i*this.sprite.width, this.sprite.height/2,this.rotation);
 	}
 	/*
 	this.sprite.drawWrappedCentredAt(
