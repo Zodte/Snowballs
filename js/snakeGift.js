d1842fa6406fd3bf2d134038de4c6f4e797f2864
duc tran <kristjantran@yahoo.com>
2015-11-25 13:55:47 +0000

    added collision, stardust

--- js/snakeGift.js
+++ js/snakeGift.js
@@ -2,18 +2,42 @@ function snakeGift(descr) {
 	this.setup(descr);
 	
 	this.sprite = this.sprite || g_sprites.snakeGift;
+	this.oriScale = this.sprite.scale;
+	this.scale = this.oriScale;
 };
 
 snakeGift.prototype = new Entity();
-snakeGift.prototype.cx = 800;
-snakeGift.prototype.cy = 300;
-snakeGift.prototype.vel = 2;
+
+snakeGift.prototype.vel = 1;
+snakeGift.prototype.damage = this.vel*10;
+snakeGift.prototype.enemy = true;
+
 snakeGift.prototype.update = function(du) {
-	//this.cx -= this.vel;
-	this.computeFloatingStep(du);
-	//this.computeSubStep(du);
+
 	spatialManager.unregister(this);
 	
+	this.computeFloatingStep(du);
+	if(this.cx < -this.getRadius()) return entityManager.KILL_ME_NOW;
+	
+	if(this.enemy){
+		var hitEntity = this.findHitEntity();
+		if (hitEntity) {
+			var canGetEnemyHit = hitEntity.getEnemyHit;
+			if (canGetEnemyHit) {
+				canGetEnemyHit.call(hitEntity, this.damage); 
+				return entityManager.KILL_ME_NOW;
+			}
+		}
+	}else{
+		var hitEntity = this.findHitEntity();
+		if (hitEntity) {
+			var canGift = hitEntity.takeGift;
+			if (canGift) {
+				canGift.call(hitEntity, this.gift); 
+				return entityManager.KILL_ME_NOW;
+			}
+		}
+	}
 	spatialManager.register(this);
 };
 
@@ -22,28 +46,38 @@ snakeGift.prototype.computeFloatingStep = function(du) {
 	var val1 = Math.sin(this.cx*Math.PI/0.1);
 	//var val2 = Math.cos(this.cx*Math.PI/0.5);
 	var nextX = this.cx - this.vel*du;
-	var nextY = this.cy + val1/0.8*du;
+	var nextY = this.cy + val1/0.6*du;
 	this.cx = nextX;
 	this.cy = nextY;
 };
-/*
-snakeGift.prototype.computeSubStep = function (du) {
-	var values = Math.cos((this.cx % 100-50)/50*Math.PI)
-	var values2 = Math.sin((this.cx % 100-50)/50*Math.PI*-1);
-    var nextX = this.cx + this.vel * du;
-    var nextY = this.cyStart + values * 20; 
-    this.cx = nextX;
-    this.cy = nextY;
-	//this.rotation = (((values2-1)/4)-0.75)*Math.PI;
+
+snakeGift.prototype.getSnowballHit = function(damage){
+	if(this.enemy){
+		this.vel = MAP_SPEED/2;
+		this.computeFloatingStep();
+		this.scale = this.scale/1.3;
+		this.enemy = !this.enemy;
+	}
+}
+
+snakeGift.prototype.createStarDust = function() {
+	entityManager.generateStardust({
+		cx 	: this.cx,
+		cy 	: util.randRange(this.cy - this.getRadius(), this.cy+this.getRadius()),
+		velX: this.velX,
+		velY: this.velY
+	})
 };
-*/
+
 snakeGift.prototype.getRadius = function() {
 	return this.sprite.scale * (this.sprite.width/2);
 };
 
 snakeGift.prototype.render = function(ctx) {
-	
+	if(this.enemy && util.randRange(0,1) > 0.7)	this.createStarDust();
+	this.sprite.scale = this.scale;	
 	this.sprite.drawCentredAt(
 	ctx, this.cx, this.cy, this.rotation
 	);
+	this.sprite.scale = this.oriScale;
 };
