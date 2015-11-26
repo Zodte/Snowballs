6c1bc94b8407da7b2ed7ee5fdcb03552131f9054
duc tran <kristjantran@yahoo.com>
2015-11-26 21:52:53 +0000

    now snakegift can never go outside of boundary

--- js/Generator.js
+++ js/Generator.js
@@ -1,6 +1,6 @@
 function Generator(descr) {
 	this.setup(descr);
-	this.enemiesArray = [this.straightGift];
+	this.enemiesArray = [this.straightGift, this.snakeGift];
 };
 
 Generator.prototype = new Entity();
@@ -19,7 +19,7 @@ Generator.prototype.snakeGift = {next: 0,
 							generate: function(){
 								entityManager.generateSnakeGifts({
 								cx : g_canvas.width,
-								cy : util.randRange(30,entityManager.GROUND_HEIGHT-30)
+								cy : util.randRange(100,entityManager.GROUND_HEIGHT-30)
 								});
 							}
 							};
@@ -32,7 +32,7 @@ Generator.prototype.update = function(du) {
 	for(var i = 0; i < this.enemiesArray.length; i++){
 		var enemy = this.enemiesArray[i];
 		if(enemy.next < this.lived ) {
-			enemy.next = this.lived+50;
+			enemy.next = this.lived+250;
 		}
 		else if(enemy.next == this.lived) {
 			console.log("yo")
